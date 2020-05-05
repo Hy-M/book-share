9cb8e99788e3ce5d5626074a31dda70a7ccde6c7
Hy-M <humayraa.m@outlook.com>
2020-05-05 14:09:34 +0100

    create availableBooks component

---
+++ src/components/AvailableBooks.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <section v-for="book in availableBooks" v-bind:key="book.id">
+      <img :src="book.img" />
+      <h3>{{book.title}}</h3>
+      <p>{{book.author}}</p>
+      <p>{{book.distance}}</p>
+    </section>
+  </main>
+</template>
+
+<script>
+const booksData = require("../data.json");
+export default {
+  data() {
+    return {
+      availableBooks: booksData
+    };
+  }
+};
+</script>
+
+<style>
+main {
+  display: flex;
+  justify-content: space-evenly;
+}
+img {
+  height: 200px;
+}
+</style>
--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,16 +1,23 @@
 <template>
-  <section>
-    <form>
-      <input type="text" placeholder="Enter a title, author or keyword" />
-      <button>Search</button>
-    </form>
-    <h2>Browse available books nearby</h2>
-  </section>
+  <div>
+    <section>
+      <form>
+        <input type="text" placeholder="Enter a title, author or keyword" />
+        <button>Search</button>
+      </form>
+      <h2>Browse available books nearby</h2>
+    </section>
+    <AvailableBooks />
+  </div>
 </template>
 
 <script>
+import AvailableBooks from "@/components/AvailableBooks.vue";
 export default {
   name: "Homepage",
+  components: {
+    AvailableBooks
+  },
   props: {}
 };
 </script>
