24f15acc202501ef748b2a5b7f1f3b168678fe88
Hy-M <humayraa.m@outlook.com>
2020-05-13 20:15:02 +0100

    style profilePage

--- src/components/ProfilePage.vue
+++ src/components/ProfilePage.vue
@@ -1,31 +1,70 @@
 <template>
-  <div>
-    <p>user 1 is logged in</p>
-    <button>Log out</button>
-    <h4>My bookshelf</h4>
-    <p>a carousel of books</p>
-    <h4>Books i'm giving away</h4>
-    <p>A carousel of books</p>
-    <h2>Got a spare book lying around?</h2>
-    <p>Search for your book by entering the title or author, then select the correct one</p>
-    <form>
-      <input placeholder="enter book title or author" />
-      <button>Search</button>
-    </form>
-    <ul>
+  <main>
+    <section class="userStatus">
+      <p class="userStatus--status">user 1 is logged in</p>
+      <button class="user--btn btn">Log out</button>
+    </section>
+
+    <section class="bookshelves">
+      <h4 class="h4">My bookshelf</h4>
+      <p>a carousel of books</p>
+      <h4 class="h4">Books i'm giving away</h4>
+      <p>A carousel of books</p>
+    </section>
+
+    <section class="upload">
+      <h3 class="h3">Got a spare book lying around?</h3>
+      <p class="upload--info">
+        1) Search for your book
+        <br />2) Select the right option
+        <br />3) Upload it for others to use
+      </p>
+      <form class="upload--form form">
+        <input class="upload--form-input input" placeholder="enter book title or author" />
+        <button class="upload--form-btn btn">Search</button>
+      </form>
+    </section>
+
+    <!-- <ul>
       <li>each book result from the api would go here</li>
       <button>Add this book to give away</button>
       <li>each book result from the api would go here</li>
       <button>Add this book to give away</button>
       <li>each book result from the api would go here</li>
       <button>Add this book to give away</button>
-    </ul>
-  </div>
+    </ul>-->
+  </main>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      uploadForm: {
+        input: ""
+      },
+      books: []
+    };
+  },
+  methods: {
+    fetchBookToUpload() {}
+  }
+};
 </script>
 
 <style>
+main {
+  width: 95%;
+  margin: 0 auto;
+}
+
+.userStatus {
+  border-top: 1px solid var(--pink-color);
+  border-bottom: 1px solid var(--pink-color);
+  padding-bottom: 20px;
+}
+.upload--info {
+  line-height: 1.5rem;
+  text-align: left;
+}
 </style>
