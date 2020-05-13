0c47e39be5cfb6d1acc6043677097a7e3d4d1b24
Hy-M <humayraa.m@outlook.com>
2020-05-13 22:00:19 +0100

    create carouselComponent

---
+++ src/components/CarouselComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <carousel :perPage="3">
+    <slide>slide1</slide>
+    <slide>slide2</slide>
+    <slide>slide3</slide>
+  </carousel>
+</template>
+
+<script>
+import { Carousel, Slide } from "vue-carousel";
+export default {
+  components: { Carousel, Slide }
+};
+</script>
+
+<style></style>
--- src/components/ProfilePage.vue
+++ src/components/ProfilePage.vue
@@ -1,13 +1,14 @@
 <template>
   <main>
     <section class="userStatus">
-      <p class="userStatus--status">user 1 is logged in</p>
+      <p class="userStatus--status">Hello, user 1</p>
       <button class="user--btn btn">Log out</button>
     </section>
 
     <section class="bookshelves">
       <h4 class="h4">My bookshelf</h4>
       <p>a carousel of books</p>
+      <CarouselComponent />
       <h4 class="h4">Books i'm giving away</h4>
       <p>A carousel of books</p>
     </section>
@@ -16,11 +17,13 @@
       <h3 class="h3">Got a spare book lying around?</h3>
       <p class="upload--info">
         1) Search for your book
-        <br />2) Select the right option
-        <br />3) Upload it for others to use
+        <br />2) Select the right option <br />3) Upload it for others to use
       </p>
       <form class="upload--form form">
-        <input class="upload--form-input input" placeholder="enter book title or author" />
+        <input
+          class="upload--form-input input"
+          placeholder="enter book title or author"
+        />
         <button class="upload--form-btn btn">Search</button>
       </form>
     </section>
@@ -37,7 +40,11 @@
 </template>
 
 <script>
+import CarouselComponent from "./CarouselComponent.vue";
 export default {
+  components: {
+    CarouselComponent
+  },
   data() {
     return {
       uploadForm: {
