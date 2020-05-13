91d281b1a2f40d42a5b15fe7b743aaf07f8b3852
Hy-M <humayraa.m@outlook.com>
2020-05-13 17:26:09 +0100

    make get request to googleBooks api

--- src/api.js
+++ src/api.js
@@ -1,3 +1,11 @@
+const axios = require("axios");
+
 exports.getBooksByInput = input => {
-  console.log(input);
+  return axios
+    .get(
+      `https://www.googleapis.com/books/v1/volumes?q=intitle:${input}&maxResults=4`
+    )
+    .then(({ data }) => {
+      console.log(data);
+    });
 };
