8df679b0fcc5006357c2828549b729186342447a
Hy-M <humayraa.m@outlook.com>
2020-05-31 08:28:30 +0100

    display browse page boooks through api

--- src/components/AvailableBooks.vue
+++ src/components/AvailableBooks.vue
@@ -2,20 +2,17 @@
   <div>
     <main class="availableBooks main">
       <h3 class="availableBooks--h3 h3">Browse books for sharing near you</h3>
-      <div
-        class="availableBooks--book"
-        v-for="book in availableBooks"
-        v-bind:key="book.id"
-      >
-        <router-link :to="`/browse/${book.id.toString()}`">
-          <img class="availableBooks--book-img imgPreview" :src="book.img" />
+      <div class="availableBooks--book" v-for="book in availableBooks" v-bind:key="book.id">
+        <router-link :to="`/browse/${book.id}`">
+          <img
+            class="availableBooks--book-img imgPreview"
+            :src="book.volumeInfo.imageLinks.smallThumbnail"
+          />
 
-          <h4 class="availableBooks--book-h4 book--title">{{ book.title }}</h4>
+          <h4 class="availableBooks--book-h4 book--title">{{ book.volumeInfo.title }}</h4>
         </router-link>
-        <p class="availableBooks--book-info book--author">{{ book.author }}</p>
-        <p class="availableBooks--book-info book--distance">
-          {{ book.distance }}
-        </p>
+        <p class="availableBooks--book-info book--author">{{book.volumeInfo.authors[0]}}</p>
+        <p class="availableBooks--book-info book--distance">distance</p>
       </div>
     </main>
   </div>
@@ -28,7 +25,7 @@ import * as api from "../api.js";
 export default {
   data() {
     return {
-      availableBooks: booksData,
+      availableBooks: []
     };
   },
   methods: {
@@ -38,8 +35,30 @@ export default {
       //     console.log(result, "< result");
       //   });
     },
+    fetchAllSellingBooks() {
+      api.getAllSellingBooks().then(({ body }) => {
+        let availableBookTitles = [];
+        while (availableBookTitles.length < 3) {
+          let randomNum = Math.floor(Math.random() * body.length);
+          if (body[randomNum].Selling) {
+            availableBookTitles.push(body[randomNum].Selling[0]);
+          }
+        }
+
+        this.fetchBookByTitle(availableBookTitles);
+      });
+    },
+    fetchBookByTitle(availableBookTitles) {
+      for (let title of availableBookTitles) {
+        api.getBookByTitle(title).then(book => {
+          this.availableBooks.push(book.items[0]);
+        });
+      }
+    }
   },
-  mounted() {},
+  mounted() {
+    this.fetchAllSellingBooks();
+  }
 };
 </script>
 
