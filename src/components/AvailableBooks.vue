b359082f84781401fcdf742f018f543c2523e2e9
anisapatel <anisa_patel@live.co.uk>
2020-06-06 14:15:32 +0100

    added email for each book title to access for email integration

--- src/components/AvailableBooks.vue
+++ src/components/AvailableBooks.vue
@@ -30,55 +30,35 @@ export default {
   methods: {
     calculateDistance() {
       let postcode = this.availableBooks[0].postcode;
-      //   api.getPostcode(postcode).then((result) => {
-      //     console.log(result, "< result");
-      //   });
     },
-    async fetchAllSellingBooks() {
-      await api.getAllSellingBooks().then(books => {
-        // console.log(book);
+    fetchAllSellingBooks() {
+      api.getAllSellingBooks().then(bookData => {
         let availableBookTitles = [];
-        let result = books.body.map(book => {
-          // console.log(b, "<----single book");
-          // console.log(b.Email, "<----single book");
-          // console.log(b.Selling);
-          if (book.Selling) {
-            availableBookTitles.push(book);
+        // while (availableBookTitles.length < 3) {
+        //   let randomNum = Math.floor(Math.random() * book.body.length);
+        bookData.body.forEach(user => {
+          if (user.Selling) {
+            availableBookTitles.push({
+              user: user.User,
+              email: user.Email,
+              titles: user.Selling
+            });
           }
         });
-        console.log(availableBookTitles, "<-- avaialble book titles");
-        // while (availableBookTitles.length < 8) {
-        //   let randomNum = Math.floor(Math.random() * book.body.length);
-        //   if (book.body[randomNum].Selling) {
-        //     availableBookTitles.push({
-        //       user: book.body[randomNum].User,
-        //       title: book.body[randomNum].Selling[0]
-        //     });
-        //   }
-        // }
         this.fetchBookByTitle(availableBookTitles);
       });
     },
     fetchBookByTitle(availableBookTitles) {
       for (let item of availableBookTitles) {
-        // console.log(item);
-        item.Selling.forEach(title => {
-          // console.log(title, "<--- book arr");
-          api.getBookByTitle(title).then(details => {
+        item.titles.forEach(title => {
+          api.getBookByTitle(title).then(book => {
             this.availableBooks.push({
-              bookDetails: details.items[0]
+              user: item.user,
+              email: item.email,
+              bookDetails: book.items[0]
             });
-            // console.log(this.availableBooks, "<----");
           });
         });
-
-        // console.log(item, "<---item");
-        //   api.getBookByTitle(item.title).then(book => {
-        //     this.availableBooks.push({
-        //       user: item.user,
-        //       bookDetails: book.items[0]
-        //     });
-        //   });
       }
     }
   },
