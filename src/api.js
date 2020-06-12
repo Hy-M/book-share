b2110930601c6af1c1cbe6c6488761614f292876
Hy-M <humayraa.m@outlook.com>
2020-06-12 19:24:50 +0100

    remove old delete functions

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const baseURL =
   "https://9s48615mc2.execute-api.us-east-2.amazonaws.com/production/";
 
-export const getUser = (username) => {
+export const getUser = username => {
   return axios.get(`${baseURL}user/${username}`).then(({ data }) => {
     return data;
   });
@@ -16,13 +16,13 @@ export const updateUserDetails = (username, newAddress) => {
     });
 };
 
-export const deleteUser = (username) => {
+export const deleteUser = username => {
   return axios.delete(`${baseURL}user/${username}`).then(({ data }) => {
     return data;
   });
 };
 
-export const getPurchasedBooks = (username) => {
+export const getPurchasedBooks = username => {
   return axios.get(`${baseURL}books/${username}/purchased`).then(({ data }) => {
     return data;
   });
@@ -32,14 +32,14 @@ export const updatePurchasedBooks = (username, purchasedBookArr) => {
   //needs to be in an array even if a single book
   return axios
     .post(`${baseURL}books/${username}/purchased`, {
-      Purchased: purchasedBookArr,
+      Purchased: purchasedBookArr
     })
     .then(({ data }) => {
       return data;
     });
 };
 
-export const getSellingBooks = (username) => {
+export const getSellingBooks = username => {
   return axios.get(`${baseURL}books/${username}/selling`).then(({ data }) => {
     return data;
   });
@@ -54,37 +54,16 @@ export const updateSellingBooks = (username, sellingBookArr) => {
     });
 };
 
-export const deleteFromPurchased = (username, purchasedBook) => {
-  return axios
-    .delete(`${baseURL}books/${username}/purchased`, {
-      data: { Purchased: purchasedBook },
-    })
-    .then(({ data }) => {
-      return data;
-    });
-};
-export const deleteFromSelling = (username, sellingBook) => {
-  console.log(username, sellingBook);
-  return axios
-    .delete(`${baseURL}books/${username}/selling`, {
-      data: { Selling: sellingBook },
-    })
-    .then(({ data }) => {
-      console.log(data);
-      return data;
-    });
-};
-
 export const deleteFromCollection = (username, bookArray, collection) => {
   return axios
     .delete(`${baseURL}books/${username}/${collection.toLowerCase()}`, {
-      data: { [collection]: bookArray },
+      data: { [collection]: bookArray }
     })
     .then(({ data }) => {
       console.log(data, "data in deleteFromCOllection");
       return data;
     })
-    .catch((err) => console.log(err, "err in deleteFromCollection"));
+    .catch(err => console.log(err, "err in deleteFromCollection"));
 };
 
 export const getAllSellingBooks = () => {
@@ -93,11 +72,7 @@ export const getAllSellingBooks = () => {
   });
 };
 
-export const getBooksByInput = (input) => {
-  console.log(input);
-};
-
-export const getBookByTitle = (input) => {
+export const getBookByTitle = input => {
   return axios
     .get(
       `https://www.googleapis.com/books/v1/volumes?q=intitle:${input}&maxResults=1`
@@ -105,7 +80,7 @@ export const getBookByTitle = (input) => {
     .then(({ data }) => {
       return data;
     })
-    .catch((err) => console.log(err, "< err in getBookByTitle"));
+    .catch(err => console.log(err, "< err in getBookByTitle"));
 };
 
 export const getBookToUpload = (title, author) => {
@@ -120,19 +95,19 @@ export const getBookToUpload = (title, author) => {
         return Promise.reject("Book not found");
       }
     })
-    .catch((err) => console.log(err, "< err in getBookToUpload"));
+    .catch(err => console.log(err, "< err in getBookToUpload"));
 };
 
-export const validatePostcode = (postcode) => {
+export const validatePostcode = postcode => {
   return axios
     .get(`https://api.postcodes.io/postcodes/${postcode}/validate`)
     .then(({ data }) => {
       return data;
     })
-    .catch((err) => console.log(err, "< err in validatePostcode"));
+    .catch(err => console.log(err, "< err in validatePostcode"));
 };
 
-export const getDistance = (postcode) => {
+export const getDistance = postcode => {
   return axios.get(`${postcode}`).then(({ data }) => {
     console.log(data, "< data");
     return data;
