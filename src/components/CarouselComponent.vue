f685cba0005a0ae2e450c4edf49e0b74b57a005b
Hy-M <humayraa.m@outlook.com>
2020-06-12 15:09:48 +0100

    add delete functionality

--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -10,7 +10,7 @@
     :paginationColor="'var(--grey-color)'"
     :paginationActiveColor="'#d65a85'"
   >
-    <slide v-for="obj in images" :key="obj.id">
+    <slide v-for="obj in images" :key="obj.title" :id="obj.title">
       <img class="imgPreview" :src="obj.img" />
       <button class="carousel--delBtn btn" v-on:click="deleteBook">
         <i class="fas fa-trash"></i>
@@ -21,40 +21,34 @@
 
 <script>
 import { Carousel, Slide } from "vue-carousel";
+import * as api from "../api.js";
+
 export default {
   components: { Carousel, Slide },
   props: {
     images: {
       type: Array
+    },
+    username: {
+      type: String
+    },
+    status: {
+      type: String
     }
   },
   data() {
-    return {
-      slides: [
-        {
-          imgUrl:
-            "https://marketplace.canva.com/EADanktU9AE/1/0/251w/canva-green-beach-photo-book-cover-o2wPCwYqW2w.jpg",
-          id: "1",
-          title: "The sun in his eyes"
-        },
-        {
-          imgUrl:
-            "https://marketplace.canva.com/EADaiDo2aSo/1/0/251w/canva-yellow-lemon-children-book-cover-Fb1rBcVIu2U.jpg",
-          id: "2",
-          title: "The happy lemon"
-        },
-        {
-          imgUrl:
-            "https://external-preview.redd.it/Y7aBLF2UAb8iDEl4v9nXSn_-RB7BolZFjneuOhAu1f0.jpg?auto=webp&s=0423b4b424bf8d4ffc6a857e4258bc49bd97098a",
-          id: "3",
-          title: "The great gatsby"
-        }
-      ]
-    };
+    return {};
   },
   methods: {
     deleteBook(e) {
-      console.log(e, "e");
+      console.log(this.status, "status");
+      let bookToDelete = [e.target.parentElement.id];
+      return api
+        .deleteFromCollection(this.username, bookToDelete, this.status)
+        .then(resp => {
+          console.log(resp, "< deleted :0");
+        })
+        .catch(err => console.log(err, "err in deleteBook"));
     }
   },
   mounted() {}
