faa5edb141e7e0aa4c8cb20817389a46387ae4ac
Hy-M <humayraa.m@outlook.com>
2020-06-26 16:21:24 +0100

    remove commented out code

--- src/components/BooksList.vue
+++ src/components/BooksList.vue
@@ -1,37 +1,5 @@
 <template>
   <main class="main">
-    <!-- <section>
-      <h3 class="availableBooks--h3 h3">Browse books for sharing near you</h3>
-      <p v-if="this.loading">Loading</p>
-      <section class="availableBooks--all" v-if="this.availableBooks.length >= 1">
-        <div
-          class="availableBooks--book"
-          v-for="(book, index) of availableBooks"
-          v-bind:key="index"
-        >
-          <router-link :to="`/browse/${book.user}/${book.bookDetails.title}`">
-            <img
-              class="availableBooks--book-img imgPreview"
-              :src="book.bookDetails.imageLinks.smallThumbnail"
-            />
-            <h4 class="availableBooks--book-h4 book--title">{{ book.bookDetails.title }}</h4>
-          </router-link>
-          <p class="availableBooks--book-info book--author">{{ book.bookDetails.authors[0] }}</p>
-
-          <p
-            class="availableBooks--book-info book--subText"
-          >{{ book.address || "Distance unknown" }}</p>
-        </div>
-      </section>
-      <section
-        v-else-if="
-            !this.loading && !this.error && !this.availableBooks.length
-          "
-      >
-        <p>Sorry, we can't find any available books right now.</p>
-      </section>
-    </section>-->
-
     <div class="availableBooks--book">
       <router-link :to="`/browse/${book.user}/${book.distance}/${book.bookDetails.title}`">
         <img
