15bca2f429ffcbc709e35ac41b6f9e718ed57a2d
Hy-M <humayraa.m@outlook.com>
2020-06-26 17:14:20 +0100

    destrcuture env variables

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 const baseURL =
   "https://9s48615mc2.execute-api.us-east-2.amazonaws.com/production/";
-import * as config from "../config";
+// import * as config from "../config";
+const { MAPBOX_API_KEY, DISTANCEMATRIX_API_KEY } = require("../config");
 
-export const getUser = (username) => {
+export const getUser = username => {
   return axios.get(`${baseURL}user/${username}`).then(({ data }) => {
     return data;
   });
@@ -17,13 +18,13 @@ export const updateUserDetails = (username, newAddress) => {
     });
 };
 
-export const deleteUser = (username) => {
+export const deleteUser = username => {
   return axios.delete(`${baseURL}user/${username}`).then(({ data }) => {
     return data;
   });
 };
 
-export const getPurchasedBooks = (username) => {
+export const getPurchasedBooks = username => {
   return axios.get(`${baseURL}books/${username}/purchased`).then(({ data }) => {
     return data;
   });
@@ -33,14 +34,14 @@ export const updatePurchasedBooks = (username, purchasedBookArr) => {
   //needs to be in an array even if a single book
   return axios
     .post(`${baseURL}books/${username}/purchased`, {
-      Purchased: purchasedBookArr,
+      Purchased: purchasedBookArr
     })
     .then(({ data }) => {
       return data;
     });
 };
 
-export const getSellingBooks = (username) => {
+export const getSellingBooks = username => {
   return axios.get(`${baseURL}books/${username}/selling`).then(({ data }) => {
     return data;
   });
@@ -58,12 +59,12 @@ export const updateSellingBooks = (username, sellingBookArr) => {
 export const deleteFromCollection = (username, bookArray, collection) => {
   return axios
     .delete(`${baseURL}books/${username}/${collection.toLowerCase()}`, {
-      data: { [collection]: bookArray },
+      data: { [collection]: bookArray }
     })
     .then(({ data }) => {
       return data;
     })
-    .catch((err) => console.log(err, "err in deleteFromCollection"));
+    .catch(err => console.log(err, "err in deleteFromCollection"));
 };
 
 export const getAllSellingBooks = () => {
@@ -72,7 +73,7 @@ export const getAllSellingBooks = () => {
   });
 };
 
-export const getBookByTitle = (input) => {
+export const getBookByTitle = input => {
   return axios
     .get(
       `https://www.googleapis.com/books/v1/volumes?q=intitle:${input}&maxResults=1`
@@ -80,7 +81,7 @@ export const getBookByTitle = (input) => {
     .then(({ data }) => {
       return data;
     })
-    .catch((err) => console.log(err, "< err in getBookByTitle"));
+    .catch(err => console.log(err, "< err in getBookByTitle"));
 };
 
 export const getBookToUpload = (title, author) => {
@@ -95,49 +96,49 @@ export const getBookToUpload = (title, author) => {
         return Promise.reject("Book not found");
       }
     })
-    .catch((err) => console.log(err, "< err in getBookToUpload"));
+    .catch(err => console.log(err, "< err in getBookToUpload"));
 };
 
-export const validatePostcode = (postcode) => {
+export const validatePostcode = postcode => {
   return axios
     .get(`https://api.postcodes.io/postcodes/${postcode}/validate`)
     .then(({ data }) => {
       return data;
     })
-    .catch((err) => console.log(err, "< err in validatePostcode"));
+    .catch(err => console.log(err, "< err in validatePostcode"));
 };
 
 export const getDistance = (srcLat, srcLng, desLat, desLng) => {
   return axios
     .get(
-      `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${srcLat},${srcLng}&destinations=${desLat},${desLng}&key=${config.DISTANCEMATRIX_API_KEY}`
+      `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${srcLat},${srcLng}&destinations=${desLat},${desLng}&key=${DISTANCEMATRIX_API_KEY}`
     )
     .then(({ data }) => {
       return data;
     })
-    .catch((err) => console.log(err, "< err in getDistance"));
+    .catch(err => console.log(err, "< err in getDistance"));
 };
 
 export const getPostcodeByCoords = (lat, lng) => {
   return axios
     .get(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?types=postcode&access_token=${config.MAPBOX_API_KEY}`
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?types=postcode&access_token=${MAPBOX_API_KEY}`
     )
     .then(({ data }) => {
       return data;
     })
-    .catch((err) => console.log(err, "< err in getPostcodeByCoords"));
+    .catch(err => console.log(err, "< err in getPostcodeByCoords"));
 };
 
-export const getCoordsByPostcode = (postcode) => {
+export const getCoordsByPostcode = postcode => {
   return axios
     .get(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${postcode}.json?types=postcode&access_token=${config.MAPBOX_API_KEY}`
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${postcode}.json?types=postcode&access_token=${MAPBOX_API_KEY}`
     )
     .then(({ data }) => {
       return data;
     })
-    .catch((err) => console.log(err, "< err in getCoordsByPostcode"));
+    .catch(err => console.log(err, "< err in getCoordsByPostcode"));
 };
 
 // Database tree schema
