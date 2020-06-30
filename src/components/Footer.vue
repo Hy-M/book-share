ded394ecdefa34a67e350bd1d3ca34764333e316
Hy-M <humayraa.m@outlook.com>
2020-06-30 13:41:50 +0100

    improve styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,18 +12,18 @@ export default {
 
 <style>
 .footer {
-  margin: 8rem 0 1rem 0;
+  margin: 9rem 0 1rem 0;
 }
 
 @media (min-width: 768px) {
   .footer {
-    margin-top: 9rem;
+    margin-top: 11rem;
   }
 }
 
 @media (min-width: 1024px) {
   .footer {
-    margin-top: 11rem;
+    margin-top: 15rem;
   }
 }
 </style>
