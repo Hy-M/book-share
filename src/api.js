c4e25489096ddbc756563a886167f5427d0bc0dc
Hy-M <humayraa.m@outlook.com>
2020-06-30 13:46:03 +0100

    tidy api.js

--- src/api.js
+++ src/api.js
@@ -22,23 +22,6 @@ export const deleteUser = username => {
 	});
 };
 
-export const getPurchasedBooks = username => {
-	return axios.get(`${baseURL}books/${username}/purchased`).then(({ data }) => {
-		return data;
-	});
-};
-
-export const updatePurchasedBooks = (username, purchasedBookArr) => {
-	//needs to be in an array even if a single book
-	return axios
-		.post(`${baseURL}books/${username}/purchased`, {
-			Purchased: purchasedBookArr
-		})
-		.then(({ data }) => {
-			return data;
-		});
-};
-
 export const getSellingBooks = username => {
 	return axios.get(`${baseURL}books/${username}/selling`).then(({ data }) => {
 		return data;
@@ -46,7 +29,6 @@ export const getSellingBooks = username => {
 };
 
 export const updateSellingBooks = (username, sellingBookArr) => {
-	//needs to be in an array even if a single book
 	return axios
 		.post(`${baseURL}books/${username}/selling`, { Selling: sellingBookArr })
 		.then(({ data }) => {
@@ -98,56 +80,12 @@ export const getBookToUpload = (title, author) => {
 };
 
 export const validatePostcode = postcode => {
-	console.log(postcode, "postcode in validatePostcode in api");
 	return axios
 		.get(`https://api.postcodes.io/postcodes/${postcode}`)
 		.then(({ data }) => {
 			return data;
 		})
 		.catch(err => console.log(err, "< err in validatePostcode"));
-};
-
-export const getDistance = (srcLat, srcLng, desLat, desLng) => {
-	return axios
-		.get(
-			`https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${srcLat},${srcLng}&destinations=${desLat},${desLng}&key=${process.env.VUE_APP_DISTANCEMATRIX_API_KEY}`
-		)
-		.then(({ data }) => {
-			return data;
-		})
-		.catch(err => console.log(err, "< err in getDistance"));
-};
-
-export const getPostcodeByCoords = (lat, lng) => {
-	return (
-		axios
-			.get(
-				`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&result_type=postal_code&key=${process.env.VUE_APP_GOOGLE_DISTANCE_API_KEY}`
-			)
-			// .get(
-			//   `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?types=postcode&access_token=${process.env.VUE_APP_MAPBOX_API_KEY}`
-			// )
-			.then(({ data }) => {
-				return data;
-			})
-			.catch(err => console.log(err, "< err in getPostcodeByCoords"))
-	);
-};
-
-export const getCoordsByPostcode = postcode => {
-	return (
-		axios
-			.get(
-				`https://maps.googleapis.com/maps/api/geocode/json?address=${postcode}&key=${process.env.VUE_APP_GOOGLE_DISTANCE_API_KEY}`
-			)
-			// .get(
-			//   `https://api.mapbox.com/geocoding/v5/mapbox.places/${postcode}.json?types=postcode&access_token=${process.env.VUE_APP_MAPBOX_API_KEY}`
-			// )
-			.then(({ data }) => {
-				return data;
-			})
-			.catch(err => console.log(err, "< err in getCoordsByPostcode"))
-	);
 };
 
 // Database tree schema
